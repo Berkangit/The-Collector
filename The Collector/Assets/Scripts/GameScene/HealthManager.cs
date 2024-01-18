a7a2ff42ed42a9fbc6eda4e19e97f7dfa8ef0203
Berkan Taştekin <berkantastekinn@hotmail.com>
2024-01-18 20:05:30 +0300

    Health image added and health system completed

---
+++ The Collector/Assets/Scripts/GameScene/HealthManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    [SerializeField] private int health = 3;
+    [SerializeField] private int numberOfHearts = 3;
+
+    public Image[] hearts;
+
+    public Sprite fullHeart;
+
+    private void Update()
+    {
+        for(int i = 0; i< hearts.Length ; i++)
+        {
+            
+            if( i< numberOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+
+}
