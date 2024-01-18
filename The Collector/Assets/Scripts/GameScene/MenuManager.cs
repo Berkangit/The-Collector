cd1073e2100cdc1bb84487890761b244f167fc6c
Berkan Taştekin <berkantastekinn@hotmail.com>
2024-01-18 16:19:27 +0300

    Progress slider completed

--- The Collector/Assets/Scripts/GameScene/MenuManager.cs
+++ The Collector/Assets/Scripts/GameScene/MenuManager.cs
@@ -10,8 +10,15 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI tapToStartText;
     [SerializeField] private GameObject handIcon;
 
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Transform finishLineTransform;
+    [SerializeField] private Slider slider;
+
+    private float maxDistance;
     private void Start()
     {
+        maxDistance = getDistance();
+
         tapToStartText.transform.
             DOScale(1.1f, 0.5f).
             SetLoops(10000, LoopType.Yoyo).
@@ -21,5 +28,24 @@ public class MenuManager : MonoBehaviour
             DOAnchorPosX(-12f, 1f) 
             .SetLoops(100000, LoopType.Yoyo) 
             .SetEase(Ease.InOutFlash);
+    }
+
+    void Update()
+    {
+        if (playerTransform.position.z <= maxDistance && playerTransform.position.z <= finishLineTransform.position.z)
+        {
+            float distance = 1 - (getDistance() / maxDistance);
+            setProgresss(distance);
+        }
+    }
+
+    private float getDistance()
+    {
+        return Vector3.Distance(playerTransform.position, finishLineTransform.position);
+    }
+
+    private void setProgresss(float p)
+    {
+        slider.value = p;
     }
 }
