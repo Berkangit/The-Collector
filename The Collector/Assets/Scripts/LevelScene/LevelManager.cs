9f5edfc137fe7b6246bb97675adb25c556e9a940
Berkan Taştekin <berkantastekinn@hotmail.com>
2024-01-22 23:44:15 +0300

    Dance animation added and player dancing in level selection menu

--- The Collector/Assets/Scripts/LevelScene/LevelManager.cs
+++ The Collector/Assets/Scripts/LevelScene/LevelManager.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-    
-    
+    [SerializeField] private Animator animator;
+    [SerializeField] private AnimationClip[] danceAnimClip;
+    private const string DANCE_INDEX = "DanceIndex";
+    private const string DANCE = "Dance";
 
 
+    private void Start()
+    {
+        int randomDanceIndex = Random.Range(0, 3);
+        animator.SetInteger(DANCE_INDEX, randomDanceIndex);
+        animator.SetTrigger(DANCE);
+    }
 
 }
